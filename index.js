0ee29d045b4726319a99c852a27f045b83836590
Márta Márton <daniel.marton.marta@gmail.com>
2021-03-01 18:46:26 +0100

    Made generateMaps function in maps.js and connected it with index.js feel free to rename everything cause the maps and maps.js are confusing

---
+++ index.js
@@ -0,0 +1,5 @@
+//just inporting maps.js
+const maps2 = require("./maps.js");
+
+//Time to use the functions fam
+maps2.generateMap(maps2);
--- maps.js
+++ maps.js
@@ -1,10 +1,22 @@
 const fs = require("fs");
-let segedT = [];
-let segedT2 = [];
-const file = fs.readFileSync("./maps", { encoding: "utf8" });
-segedT = file.split("\n");
-console.log(segedT);
-for (let i = 0; i < segedT.length; i++) {
-  segedT2[i] = segedT[i].split("");
-}
-console.log(segedT2);
+//NOTE: HAPPY TO RENAME ANYTHING , MAPS AND MAPS.JS A BIT CONFUSING
+//Made the code a function , now we can call it in maps.js
+const generateMap = () => {
+  //defined 2empty array for the read
+  let segedT = [];
+  let segedT2 = [];
+  //read and split with \n and "" by character
+  const file = fs.readFileSync("./maps", { encoding: "utf8" });
+  segedT = file.split("\n");
+  console.log(segedT);
+  for (let i = 0; i < segedT.length; i++) {
+    segedT2[i] = segedT[i].split("");
+  }
+  //JUst a log out to check if all GUcci my dude
+  console.log(segedT2);
+};
+
+//made the export to start and connect with index.js
+module.exports = {
+  generateMap,
+};
