ca05d47023c7437b1b3283031e5f62cf75a2f0a1
Márta Márton <daniel.marton.marta@gmail.com>
2021-03-03 10:37:44 +0100

    I DID the step () function

--- gameplay.js
+++ gameplay.js
@@ -1,13 +1,12 @@
 const stepRight = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j].icon === 'X') {
-        if (arr[i][j + 1].icon !== '#') {
-          const temp = arr[i][j + 1];
-          arr[i][j + 1] = arr[i][j];
-          arr[i][j] = temp;
-          return arr;
-        }
+      //  if (arr[i][j].icon === 'X') {
+      if (arr[i][j + 1].icon !== "#") {
+        const temp = arr[i][j + 1];
+        arr[i][j + 1] = arr[i][j];
+        arr[i][j] = temp;
+        return arr;
       }
     }
   }
@@ -18,8 +17,8 @@ const stepRight = (arr) => {
 const stepDown = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j].icon === 'X') {
-        if (arr[i + 1][j].icon !== '#') {
+      if (arr[i][j].icon === "X") {
+        if (arr[i + 1][j].icon !== "#") {
           const temp = arr[i + 1][j];
           arr[i + 1][j] = arr[i][j];
           arr[i][j] = temp;
@@ -35,8 +34,8 @@ const stepDown = (arr) => {
 const stepLeft = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j].icon === 'X') {
-        if (arr[i][j - 1].icon !== '#') {
+      if (arr[i][j].icon === "X") {
+        if (arr[i][j - 1].icon !== "#") {
           const temp = arr[i][j - 1];
           arr[i][j - 1] = arr[i][j];
           arr[i][j] = temp;
@@ -52,8 +51,8 @@ const stepLeft = (arr) => {
 const stepUp = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-      if (arr[i][j].icon === 'X') {
-        if (arr[i - 1][j].icon !== '#') {
+      if (arr[i][j].icon === "X") {
+        if (arr[i - 1][j].icon !== "#") {
           const temp = arr[i - 1][j];
           arr[i - 1][j] = arr[i][j];
           arr[i][j] = temp;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //just inporting maps.js
-const maps2 = require('./maps.js');
-const step = require('./gameplay.js');
-const readlinesync = require('readline-sync');
+const maps2 = require("./maps.js");
+const step = require("./gameplay.js");
+const readlinesync = require("readline-sync");
 
 //Time to use the functions fam
 const map = maps2.generateMap();
@@ -9,29 +9,92 @@ const map = maps2.generateMap();
 const printMap = () => {
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
-      process.stdout.write(map[i][j].icon + ' ');
+      process.stdout.write(map[i][j].icon + " ");
     }
     console.log();
   }
 };
 printMap();
-let button = readlinesync.keyIn();
-while (button !== 'q') {
+//let button = readlinesync.keyIn();
+/*while (button !== "q") {
   button = readlinesync.keyIn();
-  if (button === 'd') {
+  if (button === "d") {
     step.stepRight(map);
     printMap();
   }
-  if (button === 'a') {
+  if (button === "a") {
     step.stepLeft(map);
     printMap();
   }
-  if (button === 'w') {
+  if (button === "w") {
     step.stepUp(map);
     printMap();
   }
-  if (button === 's') {
+  if (button === "s") {
     step.stepDown(map);
     printMap();
   }
-}
+}*/
+
+let x = 1;
+const step2 = () => {
+  let stringTomb = [];
+  let string;
+  for (let i = 0; i < map.length - 1; i++) {
+    for (let j = 0; j < map[i].length - 1; j++) {
+      //wallra és emtyre nem történik semmi
+      string = i + "" + j;
+      if (stringTomb.includes(string) === false) {
+        if (
+          map[i][j].type === "target" ||
+          map[i][j].type === "enemy" ||
+          map[i][j].type === "player"
+        ) {
+          if (map[i][j].direction === "RIGHT") {
+            if (map[i][j + 1].icon !== "#") {
+              const temp = map[i][j + 1];
+              map[i][j + 1] = map[i][j];
+              map[i][j] = temp;
+              string = i + "" + (j + 1);
+              stringTomb.push(string);
+              console.log(string, stringTomb);
+            }
+          }
+          if (map[i][j].direction === "DOWN") {
+            if (map[i + 1][j].icon !== "#") {
+              const temp = map[i + 1][j];
+              map[i + 1][j] = map[i][j];
+              map[i][j] = temp;
+              string = i + 1 + "" + j;
+              stringTomb.push(string);
+              console.log(string, stringTomb);
+            }
+          }
+          if (map[i][j].direction === "LEFT") {
+            if (map[i][j - 1].icon !== "#") {
+              const temp = map[i][j - 1];
+              map[i][j - 1] = map[i][j];
+              map[i][j] = temp;
+              string = i + "" + (j - 1);
+              stringTomb.push(string);
+              console.log(string, stringTomb);
+            }
+          }
+          if (map[i][j].direction === "UP") {
+            if (map[i - 1][j].icon !== "#") {
+              const temp = map[i - 1][j];
+              map[i - 1][j] = map[i][j];
+              map[i][j] = temp;
+              string = i - 1 + "" + (j + 1);
+              stringTomb.push(string);
+              console.log(string, stringTomb);
+            }
+          }
+        }
+      }
+    }
+  }
+  printMap();
+};
+
+setInterval(step2, 1000);
