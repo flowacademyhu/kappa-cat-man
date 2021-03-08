11025d3ba228ba27e583dd99ffa86179be7acd43
Márta Márton <daniel.marton.marta@gmail.com>
2021-03-08 23:45:01 +0100

    revert back

--- highscore.js
+++ highscore.js
@@ -9,20 +9,27 @@ const generateHighScore = () => {
   //read and split with \n and "" by character
   const file = fs.readFileSync("./highscore", { encoding: "utf8" });
   segedTomb = file.split(",");
-  for (let i = 1; i < segedTomb.length; i += 2) {
+  for (let i = 1; i < segedTomb.length; i++) {
     segedTomb[i] = parseInt(segedTomb[i]);
+    i += 1;
   }
-  let mentettTomb = minimumSelectionSort(segedTomb);
-  fs.writeFile("highscore", mentettTomb[0], (err) => {
+  fs.writeFile("highscore", "", (err) => {
     // In case of a error throw err.
     if (err) throw err;
   });
-
-  for (let i = 1; i < mentettTomb.length; i++) {
-    fs.appendFile("highscore", "," + mentettTomb[i], (err) => {
-      // In case of a error throw err.
-      if (err) throw err;
-    });
+  let mentettTomb = minimumSelectionSort(segedTomb);
+  for (let i = 0; i < mentettTomb.length; i++) {
+    if (i === 0) {
+      fs.appendFile("highscore", mentettTomb[i], (err) => {
+        // In case of a error throw err.
+        if (err) throw err;
+      });
+    } else {
+      fs.appendFile("highscore", "," + mentettTomb[i], (err) => {
+        // In case of a error throw err.
+        if (err) throw err;
+      });
+    }
   }
   //console.log("A LEGMAGASABB PONTOK", typeof segedTomb[0]);
   return mentettTomb;
@@ -31,8 +38,8 @@ const generateHighScore = () => {
 
 //let mentettTomb = minimumSelectionSort();
 const minimumSelectionSort = (src) => {
-  for (let i = 1; i < src.length; i = i + 2) {
-    parseInt(src[i]);
+  for (let i = 1; i < highScore.length; i = i + 2) {
+    highScore[i] = parseInt(highScore[i]);
   }
   for (let i = 1; i < src.length - 2; i += 2) {
     let minIndex = i;
@@ -45,64 +52,15 @@ const minimumSelectionSort = (src) => {
       let temp = src[minIndex];
       src[minIndex] = src[i];
       src[i] = temp;
-      let temp2 = src[minIndex - 1];
+      temp = src[minIndex - 1];
       src[minIndex - 1] = src[i - 1];
-      src[i - 1] = temp2;
+      src[i - 1] = temp;
     }
   }
-
-  /*let seged1 = [];
-  let seged2 = [];
-  for (let i = 0; i < src.length; i++) {
-    if (i % 2 !== 0) {
-      seged1.push(parseInt(src[i]));
-
-      seged2.push(src[i - 1]);
-    }
-  }
-  for (let i = seged1.length - 1; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (seged1[j] > seged1[j + 1]) {
-        const temp = seged1[j];
-        seged1[j] = seged1[j + 1];
-        seged1[j + 1] = temp;
-        const temp2 = seged2[j];
-        seged2[j] = seged2[j + 1];
-        seged2[j + 1] = temp2;
-      }
-    }
-  }
-  let mentettTomb = [];
-  for (let i = 0; i < seged1.length; i++) {
-    mentettTomb.push(seged2[i]);
-    mentettTomb.push(seged1[i]);
-  }*/
-  /*for (let i = 1; i < src.length; i = i + 2) {
-    src[i] = parseInt(src[i]);
-  }
-  for (let i = 1; i < src.length - 2; i += 2) {
-    let minIndex = i;
-    for (let j = i + 2; j < src.length; j += 2) {
-      if (src[j] < src[minIndex]) {
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      let temp = src[minIndex];
-      src[minIndex] = src[i];
-      src[i] = temp;
-      let temp2 = src[minIndex - 1];
-      src[minIndex - 1] = src[i - 1];
-      src[i - 1] = temp2;
-    }
-  }*/
-
-  //  console.log(seged1);
-  //console.log(seged2);
   return src;
 };
 
-//let highScore = [];
+let highScore = [];
 //highScore = generateHighScore();
 
 //console.log(generateHighScore());
@@ -110,10 +68,10 @@ const minimumSelectionSort = (src) => {
 //console.log(highScore);
 //segedTomb.push("7");
 
-/*for (let i = 1; i < highScore.length; i++) {
+for (let i = 1; i < highScore.length; i++) {
   highScore[i] = parseInt(highScore[i]);
   i += 1;
-}*/
+}
 
 /*if (parseInt(highScore[highScore.length - 1]) !== NaN) {
   highScore[highScore.length] = "Béla";
