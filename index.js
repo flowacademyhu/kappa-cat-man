0f9351f2e4767869cab13ca1a3e6822fbf2160c0
Varga Imre <vargaimi990330@gmail.com>
2021-03-09 14:57:53 +0100

    After lose/win, you can exit.
    LIDL solution

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //just inporting maps.js
-const maps2 = require("./maps.js");
-const step = require("./gameplay.js");
-const menu = require("./menu.js");
-const highS = require("./highscore.js");
-const readlinesync = require("readline-sync");
-const menuwin = require("./menuwin.js");
-const term = require("terminal-kit").terminal;
-const fs = require("fs");
+const maps2 = require('./maps.js');
+const step = require('./gameplay.js');
+const menu = require('./menu.js');
+const highS = require('./highscore.js');
+const readlinesync = require('readline-sync');
+const menuwin = require('./menuwin.js');
+const term = require('terminal-kit').terminal;
+const fs = require('fs');
 
 //Time to use the functions fam
 const map = maps2.generateMap();
@@ -16,19 +16,19 @@ let tombXD = [];
 const printMap = () => {
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
-      if (map[i][j].icon === "X") {
-        term.red("🐈");
-      } else if (map[i][j].icon === "#") {
-        term.red("🧱");
-      } else if (map[i][j].icon === "T") {
-        term.red("🐁");
-      } else if (map[i][j].icon === "E") {
-        term.red("👮");
+      if (map[i][j].icon === 'X') {
+        term.red('🐈');
+      } else if (map[i][j].icon === '#') {
+        term.red('🧱');
+      } else if (map[i][j].icon === 'T') {
+        term.red('🐁');
+      } else if (map[i][j].icon === 'E') {
+        term.red('👮');
       } else {
-        term.red(map[i][j].icon + " ");
+        term.red(map[i][j].icon + ' ');
       }
     }
-    term("\n");
+    term('\n');
   }
 };
 
@@ -42,45 +42,45 @@ const addAssincronListener = () => {
 
   stdin.setRawMode(true); // Ne várjon enterre
   stdin.resume(); // Csak process.exit-el lehet kilépni
-  stdin.setEncoding("utf8"); // Karaktereket kapjunk vissza
-  stdin.on("data", (key) => {
+  stdin.setEncoding('utf8'); // Karaktereket kapjunk vissza
+  stdin.on('data', (key) => {
     // Callback függvény
-    console.log("You pressed: ", key);
-    if (key === "q") {
+    console.log('You pressed: ', key);
+    if (key === 'q') {
       process.exit();
     }
-    if (key === "a") {
+    if (key === 'a') {
       for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
-          if (map[i][j].type === "player") {
-            map[i][j].direction = "LEFT";
+          if (map[i][j].type === 'player') {
+            map[i][j].direction = 'LEFT';
           }
         }
       }
     }
-    if (key === "s") {
+    if (key === 's') {
       for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
-          if (map[i][j].type === "player") {
-            map[i][j].direction = "DOWN";
+          if (map[i][j].type === 'player') {
+            map[i][j].direction = 'DOWN';
           }
         }
       }
     }
-    if (key === "d") {
+    if (key === 'd') {
       for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
-          if (map[i][j].type === "player") {
-            map[i][j].direction = "RIGHT";
+          if (map[i][j].type === 'player') {
+            map[i][j].direction = 'RIGHT';
           }
         }
       }
     }
-    if (key === "w") {
+    if (key === 'w') {
       for (let i = 0; i < map.length; i++) {
         for (let j = 0; j < map[i].length; j++) {
-          if (map[i][j].type === "player") {
-            map[i][j].direction = "UP";
+          if (map[i][j].type === 'player') {
+            map[i][j].direction = 'UP';
           }
         }
       }
@@ -95,18 +95,18 @@ const addAssincronListener = () => {
 }*/
 
 const changeDirection = (i, j) => {
-  const randomDirection = ["LEFT", "UP", "DOWN", "RIGHT"];
+  const randomDirection = ['LEFT', 'UP', 'DOWN', 'RIGHT'];
   console.log(randomDirection[Math.floor(Math.random() * 4)]);
 
-  if (map[i][j].type === "target" || map[i][j].type === "enemy") {
+  if (map[i][j].type === 'target' || map[i][j].type === 'enemy') {
     map[i][j].direction = randomDirection[Math.floor(Math.random() * 4)];
-    if (map[i][j + 1].icon === "#") {
+    if (map[i][j + 1].icon === '#') {
       map[i][j].direction = randomDirection[Math.floor(Math.random() * 4)];
-    } else if (map[i + 1][j].icon === "#") {
+    } else if (map[i + 1][j].icon === '#') {
       map[i][j].direction = randomDirection[Math.floor(Math.random() * 4)];
-    } else if (map[i][j].icon === "#") {
+    } else if (map[i][j].icon === '#') {
       map[i][j].direction = randomDirection[Math.floor(Math.random() * 4)];
-    } else if (map[i][j - 1].icon !== "#") {
+    } else if (map[i][j - 1].icon !== '#') {
       map[i][j].direction = randomDirection[Math.floor(Math.random() * 4)];
     }
   }
@@ -114,58 +114,58 @@ const changeDirection = (i, j) => {
 
 pvpEvent = (i, j) => {
   //pvp kezdete Enem
-  if (map[i][j + 1].icon == "X" && map[i][j].icon === "E") {
-    map[i][j + 1].icon = "D";
+  if (map[i][j + 1].icon == 'X' && map[i][j].icon === 'E') {
+    map[i][j + 1].icon = 'D';
     map[i][j + 1].type = null;
     map[i][j + 1].direction = null;
     map[i][j + 1].color = null;
     eletero--;
   }
-  if (map[i][j].icon == "X" && map[i][j + 1].icon === "E") {
-    map[i][j].icon = "D";
+  if (map[i][j].icon == 'X' && map[i][j + 1].icon === 'E') {
+    map[i][j].icon = 'D';
     map[i][j].type = null;
     map[i][j].direction = null;
     map[i][j].color = null;
     eletero--;
   }
-  if (map[i + 1][j].icon == "X" && map[i][j].icon === "E") {
-    map[i + 1][j].icon = "D";
+  if (map[i + 1][j].icon == 'X' && map[i][j].icon === 'E') {
+    map[i + 1][j].icon = 'D';
     map[i + 1][j].type = null;
     map[i + 1][j].direction = null;
     map[i + 1][j].color = null;
     eletero--;
   }
-  if (map[i][j].icon == "X" && map[i + 1][j].icon === "E") {
-    map[i][j].icon = "D";
+  if (map[i][j].icon == 'X' && map[i + 1][j].icon === 'E') {
+    map[i][j].icon = 'D';
     map[i][j].type = null;
     map[i][j].direction = null;
     map[i][j].color = null;
     eletero--;
   } //pvp vége
   //pvp kezdete Enem
-  if (map[i][j + 1].icon == "T" && map[i][j].icon === "X") {
-    map[i][j + 1].icon = " ";
+  if (map[i][j + 1].icon == 'T' && map[i][j].icon === 'X') {
+    map[i][j + 1].icon = ' ';
     map[i][j + 1].type = null;
     map[i][j + 1].direction = null;
     map[i][j + 1].color = null;
     score++;
   }
-  if (map[i][j].icon == "T" && map[i][j + 1].icon === "X") {
-    map[i][j].icon = " ";
+  if (map[i][j].icon == 'T' && map[i][j + 1].icon === 'X') {
+    map[i][j].icon = ' ';
     map[i][j].type = null;
     map[i][j].direction = null;
     map[i][j].color = null;
     score++;
   }
-  if (map[i + 1][j].icon == "T" && map[i][j].icon === "X") {
-    map[i + 1][j].icon = " ";
+  if (map[i + 1][j].icon == 'T' && map[i][j].icon === 'X') {
+    map[i + 1][j].icon = ' ';
     map[i + 1][j].type = null;
     map[i + 1][j].direction = null;
     map[i + 1][j].color = null;
     score++;
   }
-  if (map[i][j].icon == "T" && map[i + 1][j].icon === "X") {
-    map[i][j].icon = " ";
+  if (map[i][j].icon == 'T' && map[i + 1][j].icon === 'X') {
+    map[i][j].icon = ' ';
     map[i][j].type = null;
     map[i][j].direction = null;
     map[i][j].color = null;
@@ -185,53 +185,53 @@ const step2 = () => {
     for (let j = 1; j < map[i].length - 1; j++) {
       //wallra és emtyre nem történik semmissas
 
-      string = i + "" + j;
+      string = i + '' + j;
       if (stringTomb.includes(string) === false) {
         pvpEvent(i, j);
         if (
-          map[i][j].type === "target" ||
-          map[i][j].type === "enemy" ||
-          map[i][j].type === "player"
+          map[i][j].type === 'target' ||
+          map[i][j].type === 'enemy' ||
+          map[i][j].type === 'player'
         ) {
-          if (map[i][j].direction === "RIGHT") {
-            if (map[i][j + 1].icon !== "#") {
+          if (map[i][j].direction === 'RIGHT') {
+            if (map[i][j + 1].icon !== '#') {
               const temp = map[i][j + 1];
               map[i][j + 1] = map[i][j];
               map[i][j] = temp;
-              string = i + "" + (j + 1);
+              string = i + '' + (j + 1);
               stringTomb.push(string);
             } else {
               changeDirection(i, j);
             }
           }
-          if (map[i][j].direction === "DOWN") {
-            if (map[i + 1][j].icon !== "#") {
+          if (map[i][j].direction === 'DOWN') {
+            if (map[i + 1][j].icon !== '#') {
               const temp = map[i + 1][j];
               map[i + 1][j] = map[i][j];
               map[i][j] = temp;
-              string = i + 1 + "" + j;
+              string = i + 1 + '' + j;
               stringTomb.push(string);
             } else {
               changeDirection(i, j);
             }
           }
-          if (map[i][j].direction === "LEFT") {
-            if (map[i][j - 1].icon !== "#") {
+          if (map[i][j].direction === 'LEFT') {
+            if (map[i][j - 1].icon !== '#') {
               const temp = map[i][j - 1];
               map[i][j - 1] = map[i][j];
               map[i][j] = temp;
-              string = i + "" + (j - 1);
+              string = i + '' + (j - 1);
               stringTomb.push(string);
             } else {
               changeDirection(i, j);
             }
           }
-          if (map[i][j].direction === "UP") {
-            if (map[i - 1][j].icon !== "#") {
+          if (map[i][j].direction === 'UP') {
+            if (map[i - 1][j].icon !== '#') {
               const temp = map[i - 1][j];
               map[i - 1][j] = map[i][j];
               map[i][j] = temp;
-              string = i - 1 + "" + (j + 1);
+              string = i - 1 + '' + (j + 1);
               stringTomb.push(string);
             } else {
               changeDirection(i, j);
@@ -244,7 +244,7 @@ const step2 = () => {
   console.clear();
   checkTarget();
   if (eletero === 0) {
-    console.log("vesztettél");
+    console.log('vesztettél');
     clearInterval(tombXD[0]);
     console.clear();
     maps2.generateMap();
@@ -254,19 +254,19 @@ const step2 = () => {
     highS.minimumSelectionSort(highTomb);
     for (let i = 0; i < highTomb.length; i += 2) {
       if (i === highTomb.length - 2) {
-        process.stdout.write(highTomb[i] + ":" + score);
+        process.stdout.write(highTomb[i] + ':' + score);
         console.log();
       } else {
-        process.stdout.write(highTomb[i] + ":" + highTomb[i + 1]);
+        process.stdout.write(highTomb[i] + ':' + highTomb[i + 1]);
         console.log();
       }
     }
-    fs.appendFile("highscore", score, function (err) {
+    fs.appendFile('highscore', score, function (err) {
       if (err) throw err;
     });
-    let button = readlinesync.keyIn('DÖGÖLJ MEG');
-    if (button === 'y') {
-      menu.game();
+    let button = readlinesync.keyIn('You are dead. Press Q to exit.');
+    if (button === 'q') {
+      process.exit();
     }
     return;
   }
@@ -276,20 +276,20 @@ const step2 = () => {
     clearInterval(myVar);
   }*/
   printMap();
-  console.log("Your score Cat-man", score);
+  console.log('Your score Cat-man', score);
 };
 
 const checkTarget = () => {
   let counter = 0;
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
-      if (map[i][j].icon === "T") {
+      if (map[i][j].icon === 'T') {
         counter++;
       }
     }
   }
   if (counter === 0) {
-    console.log("nyertél00");
+    //console.log("nyertél00");
     // process.stdin.removeAllListeners('data');
     // process.stdin.removeAllListeners('keypress');
     // process.stdin.setRawMode(false);
@@ -303,18 +303,22 @@ const checkTarget = () => {
     highS.minimumSelectionSort(highTomb);
     for (let i = 0; i < highTomb.length; i += 2) {
       if (i === highTomb.length - 2) {
-        process.stdout.write(highTomb[i] + ":" + score);
+        process.stdout.write(highTomb[i] + ':' + score);
         console.log();
       } else {
-        process.stdout.write(highTomb[i] + ":" + highTomb[i + 1]);
+        process.stdout.write(highTomb[i] + ':' + highTomb[i + 1]);
         console.log();
       }
     }
-    fs.appendFile("highscore", score, function (err) {
+    fs.appendFile('highscore', score, function (err) {
       if (err) throw err;
     });
     //menuwin.menuAfterWin();
-    return;
+
+    let button2 = readlinesync.keyIn('Congratz you won! Press Q-t exit.');
+    if (button2 === 'q') {
+      process.exit();
+    }
   }
 };
 
