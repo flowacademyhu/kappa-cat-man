1084a212238698ab7adc3b68095aee00ec01b35a
Márta Márton <daniel.marton.marta@gmail.com>
2021-03-10 14:41:52 +0100

    Added some graphics to menu items Added some high score gucci printouts Still to do little csinositás

--- menu.js
+++ menu.js
@@ -1,53 +1,68 @@
-let readLine = require('readline-sync');
-let cfonts = require('cfonts');
-let term = require('terminal-kit').terminal;
-const startGame = require('./index');
-const highS = require('./highscore');
+let readLine = require("readline-sync");
+let cfonts = require("cfonts");
+let term = require("terminal-kit").terminal;
+const startGame = require("./index");
+const highS = require("./highscore");
 
-let name = '';
+let name = "";
 const game = (name) => {
   startGame.start(name);
 };
 
-cfonts.say('CATMAN', {
-  font: 'block',
-  align: 'center',
-  colors: ['system'],
-  background: 'transparent',
+cfonts.say("CATMAN", {
+  font: "block",
+  align: "center",
+  colors: ["system"],
+  background: "transparent",
   letterSpacing: 1,
   lineHeight: 1,
   space: true,
-  maxLength: '0',
-  gradient: 'magenta,yellow',
+  maxLength: "0",
+  gradient: "magenta,yellow",
   independentGradient: true,
   transitionGradient: true,
-  env: 'node',
+  env: "node",
 });
 
 const klari = () => {
-  term.drawImage('./klarikam.jpg', {
+  term.drawImage("./klarikam.jpg", {
     shrink: { width: 180, height: 1500 },
   });
 };
 
 const margit = () => {
-  term.drawImage('./margitom.jpg', {
+  term.drawImage("./margitom.jpg", {
     shrink: { width: 180, height: 150 },
   });
 };
 
 const items = [
-  '                😻 New Game',
-  '                😸 Highscores',
-  '                🙀 Credits',
-  '                😿 Exit',
+  "                😻 New Game",
+  "                😸 Highscores",
+  "                🙀 Credits",
+  "                😿 Exit",
 ];
 term.singleColumnMenu(items, function (error, response) {
-  term('\n').eraseLineAfter.red('You chose: %s\n', response.selectedText);
+  term("\n").eraseLineAfter.red("You chose: %s\n", response.selectedText);
   if (response.selectedIndex === 0) {
+    console.clear();
+    cfonts.say("CATMAN", {
+      font: "block",
+      align: "center",
+      colors: ["system"],
+      background: "transparent",
+      letterSpacing: 1,
+      lineHeight: 1,
+      space: true,
+      maxLength: "0",
+      gradient: "magenta,yellow",
+      independentGradient: true,
+      transitionGradient: true,
+      env: "node",
+    });
     term.grabInput(false);
-    name = readLine.question('What is your name? ');
-    console.log('Welcome, ', name, '!');
+    name = readLine.question("What is your name? ");
+    console.log("Welcome, ", name, "!");
 
     setTimeout(klari, 30);
     setTimeout(madeBy, 40);
@@ -56,31 +71,62 @@ term.singleColumnMenu(items, function (error, response) {
     let highTomb = highS.generateHighScore();
 
     highS.minimumSelectionSort(highTomb);
-    for (let i = 0; i < highTomb.length; i += 2) {
-      process.stdout.write(highTomb[i] + ':' + highTomb[i + 1]);
+    startGame.makeTable(highTomb);
+    /*for (let i = 0; i < highTomb.length; i += 2) {
+      process.stdout.write(highTomb[i] + ":" + highTomb[i + 1]);
 
       console.log();
-    }
+    }*/
 
     term.grabInput(false);
   } else if (response.selectedIndex === 2) {
-    term.grabInput(false);
     console.clear();
-    console.log('Copyright (c) 2021 Team Cica');
+    cfonts.say("KREDITSZ", {
+      font: "block",
+      align: "center",
+      colors: ["system"],
+      background: "transparent",
+      letterSpacing: 1,
+      lineHeight: 1,
+      space: true,
+      maxLength: "0",
+      gradient: "magenta,yellow",
+      independentGradient: true,
+      transitionGradient: true,
+      env: "node",
+    });
+    term.grabInput(false);
+
+    console.log("Copyright (c) 2021 Team Cica");
     console.log(
-      'Team Cica members: Mate Pinter, Jonka Szabo, Marton Marta, Barnabas Judak, Imre Varga'
+      "Team Cica members: Mate Pinter, Jonka Szabo, Marton Marta, Barnabas Judak, Imre Varga"
     );
-    console.log('Thanks for playing the game!');
-    console.log('2021, Hungary, Szeged, Flow Academy');
+    console.log("Thanks for playing the game!");
+    console.log("2021, Hungary, Szeged, Flow Academy");
     //setTimeout(margit, 1500);
   } else if (response.selectedIndex === 3) {
+    console.clear();
+    cfonts.say("IJKSZIT", {
+      font: "block",
+      align: "center",
+      colors: ["system"],
+      background: "transparent",
+      letterSpacing: 1,
+      lineHeight: 1,
+      space: true,
+      maxLength: "0",
+      gradient: "magenta,yellow",
+      independentGradient: true,
+      transitionGradient: true,
+      env: "node",
+    });
     term.grabInput(false);
     process.exit();
   }
 });
 
 const madeBy = () => {
-  console.log('made by Balázs Klári és Korda Gyuri <3');
+  console.log("made by Balázs Klári és Korda Gyuri <3");
 };
 
 module.exports = {
