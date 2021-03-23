a946f2c2cf01ec9182d098bdf06e28448c1e475b
Roland Németh <info@nemethroland.hu>
2021-03-23 09:46:47 +0100

    Change sorting algorithm

--- highscore.js
+++ highscore.js
@@ -2,15 +2,38 @@ const fs = require('fs');
 
 const HIGHSCORE_FILE = './highscore.txt'
 
+const compareScores = (a, b) => {
+  return a.score - b.score
+}
+
 const generateHighScore = () => {
   let segedTomb = [];
   const file = fs.readFileSync(HIGHSCORE_FILE, { encoding: 'utf8' });
   segedTomb = file.split(',');
 
+  const highScores = []
+
   for (let i = 1; i < segedTomb.length; i += 2) {
     segedTomb[i] = parseInt(segedTomb[i]);
+
+    const currentScore = {
+      name: segedTomb[i-1],
+      score: parseInt(segedTomb[i]), 
+    }
+
+    highScores.push(currentScore);
   }
-  return minimumSelectionSort(segedTomb);
+
+  const sortedHighScores = highScores.sort(compareScores).reverse()
+
+  const result = []
+
+  for(let i = 0; i < sortedHighScores.length; i++) {
+    result.push(sortedHighScores[i].name)
+    result.push(sortedHighScores[i].score)
+  }
+
+  return result;
 };
 
 const addHighScore = (name, score, cb) => {
@@ -21,25 +44,8 @@ const addHighScore = (name, score, cb) => {
   );
 }
 
-const minimumSelectionSort = (src) => {
-  for (let i = 1; i < src.length - 2; i += 2) {
-    let minIndex = i;
-    for (let j = i + 2; j < src.length; j += 2) {
-      if (src[j] > src[minIndex]) {
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      let temp = src[minIndex];
-      src[minIndex] = src[i];
-      src[i] = temp;
-      temp = src[minIndex - 1];
-      src[minIndex - 1] = src[i - 1];
-      src[i - 1] = temp;
-    }
-  }
-  return src;
-};
+generateHighScore()
+
 module.exports = {
   addHighScore,
   generateHighScore,
