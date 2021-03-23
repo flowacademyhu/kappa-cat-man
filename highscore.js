9cb37ad098d6afc891896a81621f6a18703fe11c
Roland Németh <info@nemethroland.hu>
2021-03-23 10:25:16 +0100

    Remove unused code

--- highscore.js
+++ highscore.js
@@ -1,35 +1,14 @@
 const fs = require('fs');
 
-const HIGHSCORE_FILE = './highscore.txt'
 const HIGHSCORE_FILE_JSON = './highscore.json'
 
 const compareScores = (a, b) => {
   return a.score - b.score
 }
 
-const readHighScoreFile = () => {
-  const file = fs.readFileSync(HIGHSCORE_FILE, { encoding: 'utf8' });
-  return file.split(',');
-}
-
 const readHighScoreFileJson = () => {
   const file = fs.readFileSync(HIGHSCORE_FILE_JSON, { encoding: 'utf8' });
   return JSON.parse(file)
-}
-
-const parseInputHighScore = (savedHighScores) => {
-  const highScores = []
-
-  for (let i = 1; i < savedHighScores.length; i += 2) {
-    const currentScore = {
-      name: savedHighScores[i-1],
-      score: parseInt(savedHighScores[i]), 
-    }
-
-    highScores.push(currentScore);
-  }
-
-  return highScores
 }
 
 const sortHighScores = (highScore) => {
